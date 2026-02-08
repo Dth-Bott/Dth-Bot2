7e47efc55c0acb681f75960cd2fbe7def7259323
dthbot <blebledarius@gmail.com>
2026-02-08 20:16:06 +0100

    Update info-ruoli.js

--- plugins/info-ruoli.js
+++ plugins/info-ruoli.js
@@ -1,31 +1,33 @@
-export async function participantsUpdate(m, { conn }) {
-  const { id, participants, action, author } = m
+const handler = m => m;
 
-  if (!id || !participants || !author) return
+handler.before = async function (message, { conn }) {
+  const chat = global.db.data.chats[message.chat] || {};
+  const detectEnabled = chat.detect;
 
-  // PROMOZIONE ADMIN
-  if (action === 'promote') {
-    for (let user of participants) {
-      await conn.sendMessage(
-        id,
-        {
-          text: `🩸 *@${author.split('@')[0]}* ha donato i poteri a *@${user.split('@')[0]}*`,
-          mentions: [author, user]
-        }
-      )
-    }
+  if (!detectEnabled) return;
+
+  // prende sempre il JID corretto di chi ha fatto l'azione
+  const sender = message.participant || message.sender;
+
+  // PROMOZIONE (admin dato)
+  if (message.messageStubType === 29) {
+    const promotedUser = message.messageStubParameters[0];
+
+    await conn.sendMessage(message.chat, {
+      text: `🩸 *@${sender.split('@')[0]}* 𝐡𝐚 𝐝𝐚𝐭𝐨 𝐢 𝐩𝐨𝐭𝐞𝐫𝐢 𝐚 *@${promotedUser.split('@')[0]}*`,
+      mentions: [sender, promotedUser]
+    });
   }
 
-  // RETROCESSIONE ADMIN
-  if (action === 'demote') {
-    for (let user of participants) {
-      await conn.sendMessage(
-        id,
-        {
-          text: `⛓️ *@${author.split('@')[0]}* ha tolto i poteri a *@${user.split('@')[0]}*`,
-          mentions: [author, user]
-        }
-      )
-    }
+  // RETROCESSIONE (admin tolto)
+  if (message.messageStubType === 30) {
+    const demotedUser = message.messageStubParameters[0];
+
+    await conn.sendMessage(message.chat, {
+      text: `⛓️ *@${sender.split('@')[0]}* 𝐡𝐚 𝐭𝐨𝐥𝐭𝐨 𝐢 𝐩𝐨𝐭𝐞𝐫𝐢 𝐚 *@${demotedUser.split('@')[0]}*`,
+      mentions: [sender, demotedUser]
+    });
   }
-}
+};
+
+export default handler;
